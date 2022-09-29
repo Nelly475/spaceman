a5d6a6a54cee79bae7a5f1142d6df2ddc0e9c1bc
Nelly475 <71159638+Nelly475@users.noreply.github.com>
2022-09-29 15:37:06 -0500

    1_8

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float jumpForce = 6f;
+    Rigidbody2D rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Jump()
+    {
+        
+    }
+}
